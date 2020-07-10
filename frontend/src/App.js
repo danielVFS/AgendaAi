53d7f32ed591d610c2074feb6ac7bedf6973ce92
danielVFS <danielv759153@gmail.com>
2020-07-10 14:05:45 -0300

    redux-persisty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -6,16 +7,18 @@ import './config/Reactotron';
 
 import GlobalStyles from './styles/global';
 import Routes from './routes';
-import store from './store';
+import { store, persistor } from './store';
 import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyles />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
