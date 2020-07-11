e60a3d6d1298d6c14b31f1d016c996976dd0fc74
danielVFS <danielv759153@gmail.com>
2020-07-11 17:07:50 -0300

    react: update perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Container } from './styles';
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector((state) => state.user.profile);
 
   function handleSubmit(data) {
-    alert(data);
+    dispatch(updateProfileRequest(data));
   }
 
   return (
