ef5a2da2c0f52ed1515caee970b1dfe2c75d0038
danielVFS <danielv759153@gmail.com>
2020-07-14 16:26:30 -0300

    rn: default button & input

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '../../components/Background';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>signin</Text>
+
+      <Input
+        style={{ marginTop: 5 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
