baae1b53fcb7d2f7e27061a2699de72f2c83b0a2
danielVFS <danielv759153@gmail.com>
2020-07-15 15:35:26 -0300

    rn: configuring redux

--- mobile/App.js
+++ mobile/App.js
@@ -2,16 +2,23 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { StatusBar } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import './src/config/Reactotron';
 import Routes from './src/routes';
+import {store,persistor} from './src/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="#240b36" barStyle="light-content" />
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#240b36" barStyle="light-content" />
+          <Routes />
+      </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
