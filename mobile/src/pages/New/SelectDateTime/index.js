16df027ce88ae2e01b00b40e87d5517a7410e91a
danielVFS <danielv759153@gmail.com>
2020-07-20 15:20:49 -0300

    rn: listing appointments

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Background from '../../../components/Background';
+
 export default function SelectDateTime() {
   return (
-    <View>
-      <Text>Select Date Time</Text>
-    </View>
+    <Background>
+      <View>
+        <Text>Select Date Time</Text>
+      </View>
+    </Background>
   );
 }
